19bbce31183cf0096bceac6331f5d61c1f93c153
Christoffer Winterkvist <chris@zenangst.com>
2014-09-27 11:08:03 +0200

    Initial Commit

---
+++ OptionalControl/OptionalControl.h
@@ -0,0 +1,16 @@
+//
+//  OptionalControl.h
+//  OptionalControl
+//
+//  Created by Christoffer Winterkvist on 27/09/14.
+//  Copyright (c) 2014 zenangst. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface OptionalControl : NSObject
+
++ (instancetype)sharedPlugin;
+
+@property (nonatomic, strong, readonly) NSBundle* bundle;
+@end
---
+++ OptionalControl/OptionalControl.m
@@ -0,0 +1,68 @@
+//
+//  OptionalControl.m
+//  OptionalControl
+//
+//  Created by Christoffer Winterkvist on 27/09/14.
+//    Copyright (c) 2014 zenangst. All rights reserved.
+//
+
+#import "OptionalControl.h"
+
+static OptionalControl *sharedPlugin;
+
+@interface OptionalControl()
+
+@property (nonatomic, strong, readwrite) NSBundle *bundle;
+@end
+
+@implementation OptionalControl
+
++ (void)pluginDidLoad:(NSBundle *)plugin
+{
+    static dispatch_once_t onceToken;
+    NSString *currentApplicationName = [[NSBundle mainBundle] infoDictionary][@"CFBundleName"];
+    if ([currentApplicationName isEqual:@"Xcode"]) {
+        dispatch_once(&onceToken, ^{
+            sharedPlugin = [[self alloc] initWithBundle:plugin];
+        });
+    }
+}
+
++ (instancetype)sharedPlugin
+{
+    return sharedPlugin;
+}
+
+- (id)initWithBundle:(NSBundle *)plugin
+{
+    if (self = [super init]) {
+        // reference to plugin's bundle, for resource access
+        self.bundle = plugin;
+        
+        // Create menu items, initialize UI, etc.
+
+        // Sample Menu Item:
+        NSMenuItem *menuItem = [[NSApp mainMenu] itemWithTitle:@"Edit"];
+        if (menuItem) {
+            [[menuItem submenu] addItem:[NSMenuItem separatorItem]];
+            NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"Do Action" action:@selector(doMenuAction) keyEquivalent:@""];
+            [actionMenuItem setTarget:self];
+            [[menuItem submenu] addItem:actionMenuItem];
+        }
+    }
+    return self;
+}
+
+// Sample Action, for menu item:
+- (void)doMenuAction
+{
+    NSAlert *alert = [NSAlert alertWithMessageText:@"Hello, World" defaultButton:nil alternateButton:nil otherButton:nil informativeTextWithFormat:@""];
+    [alert runModal];
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+@end
