5f7f9e520c96cd0e20792ed53210473eeb3200a4
Christoffer Winterkvist <chris@zenangst.com>
2014-11-01 10:31:52 +0100

    Remove boilerplate

--- OptionalControl/OptionalControl.m
+++ OptionalControl/OptionalControl.m
@@ -36,29 +36,12 @@ static OptionalControl *sharedPlugin;
 
 - (id)initWithBundle:(NSBundle *)plugin
 {
-    if (self = [super init]) {
-        // reference to plugin's bundle, for resource access
-        self.bundle = plugin;
-        
-        // Create menu items, initialize UI, etc.
+    self = [super init];
+    if (!self) return nil;
 
-        // Sample Menu Item:
-        NSMenuItem *menuItem = [[NSApp mainMenu] itemWithTitle:@"Edit"];
-        if (menuItem) {
-            [[menuItem submenu] addItem:[NSMenuItem separatorItem]];
-            NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"Do Action" action:@selector(doMenuAction) keyEquivalent:@""];
-            [actionMenuItem setTarget:self];
-            [[menuItem submenu] addItem:actionMenuItem];
-        }
-    }
+    self.bundle = plugin;
+
     return self;
-}
-
-// Sample Action, for menu item:
-- (void)doMenuAction
-{
-    NSAlert *alert = [NSAlert alertWithMessageText:@"Hello, World" defaultButton:nil alternateButton:nil otherButton:nil informativeTextWithFormat:@""];
-    [alert runModal];
 }
 
 - (void)dealloc
